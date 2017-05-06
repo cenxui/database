685b368406ffc6cfb9274051684a7f1c3343b892
cenxui <cenxuilin@gmail.com>
2017-05-06 16:24:13 +0800

    this coonction

---
+++ server/src/main/java/com/cenxui/server/listener/ApplicationProperties.java
@@ -0,0 +1,7 @@
+package com.cenxui.server.listener;
+
+/**
+ * Created by cenxui on 5/4/17.
+ */
+public class ApplicationProperties {
+}
---
+++ server/src/test/java/com/cenxui/server/listener/ApplicationPropertiesTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by cenxui on 5/4/17.
+ */
+public class ApplicationPropertiesTest {
+
+}
